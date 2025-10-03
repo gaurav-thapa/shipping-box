f879b88dcd414f750ceb6c225dfe4849811c97ff
Gaurav Thapa <thapagaurav7011@gmail.com>
2025-10-03 12:18:12 +0530

    fix for smaller width phones

--- src/Homepage.jsx
+++ src/Homepage.jsx
@@ -3,7 +3,7 @@ import { Outlet } from 'react-router'
 
 const Homepage = () => {
   return (
-    <div className='bg-gradient-to-t from-gray-950 to-gray-800 min-h-screen text-white'>
+    <div className='bg-gradient-to-t from-gray-950 to-gray-800 w-screen h-screen text-white'>
       <Navbar/>
       <Outlet/>
     </div>
